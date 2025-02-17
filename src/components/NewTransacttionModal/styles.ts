1ab75a18191f38d7ff5924fe953656ef610bc86a
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-17 16:42:23 -0300

    Estilizando do Botão de Entrada e Saída no Modal

--- src/components/NewTransacttionModal/styles.ts
+++ src/components/NewTransacttionModal/styles.ts
@@ -33,7 +33,7 @@ export const Content = styled(Dialog.Content)`
       background: ${props => props.theme["gray-900"]};
       color: ${props => props.theme["gray-300"]};
       padding: 1rem;
-      
+
       &::placeholder {
         color: ${props => props.theme["gray-500"]};
       }
@@ -68,4 +68,32 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${props => props.theme["gray-500"]};
+`;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${props => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${props => props.theme["gray-300"]};
+  
+  svg {
+    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+  }
 `;
