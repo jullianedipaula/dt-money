62e6e7256c52257e8970777e41a154e0cd0ab5a9
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-17 15:24:37 -0300

    Componentizando o Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,10 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary/>
     </div>
   );
 }
