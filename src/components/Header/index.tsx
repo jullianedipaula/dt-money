08950815b636cfdd2ec88823f49032ab3f0ee731
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-17 16:36:37 -0300

    Estilizando o Modal de Novas Transações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,24 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
 import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransacttionModal";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        
+
         <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <NewTransactionButton>Nova transação</NewTransactionButton>
-        </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay />
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-      
+
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          
+          <NewTransactionModal />
+
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   );
